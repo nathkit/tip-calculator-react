d37788b6886302d4e0c0dbd1abdc7e09c22ee8b5
Nathkit_Nae <nathkit101@gmail.com>
2023-08-25 00:06:51 +0700

    feat:created calculator  structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,64 @@ function App() {
 
   return (
     <>
-      Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
-      person Total / person Reset
-      <div class="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
+      <div className="bg-body-bg h-screen">
+        <div className="app-header">
+          <h1>S P L I</h1>
+          <h1>T T E R</h1>
+        </div>
+        <div className="calculator-container min-w-[">
+          <div className="input-container">
+            <div className="bill-input">
+              <h1>Bill</h1>
+              <input className="input" type="text" placeholder="$" />
+            </div>
+            <div className="tip-select-section">
+              <h1>Select Tip %</h1>
+              <div className="tip-btn-container">
+                <button className="tip-btn">5%</button>
+                <button className="tip-btn">10%</button>
+                <button className="tip-btn">15%</button>
+                <button className="tip-btn">25%</button>
+                <button className="tip-btn">50%</button>
+                <div className="tip-btn">
+                  <input type="text" placeholder="Custom" />
+                </div>
+              </div>
+            </div>
+            <div className="people-count">
+              <h1>Number of People</h1>
+              <input className="input" type="text" />
+            </div>
+          </div>
+          <div className="output-container">
+            <div className="calulate-output">
+              <div className="label">
+                <h1>Tip Amount</h1>
+                <p>/ person</p>
+              </div>
+              <div className="display-output">
+                <h1>$0.00</h1>
+              </div>
+            </div>
+            <div className="calulate-output">
+              <div className="label">
+                <h1>Total</h1>
+                <p>/ person</p>
+              </div>
+              <div className="display-output">
+                <h1>$0.00</h1>
+              </div>
+            </div>
+            <div className="reset-btn">RESET</div>
+          </div>
+        </div>
+        <div class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Nathkit Tangthumrongsakkul</a>.
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'body-bg': '#c4e4e7',
+      'green-dark': '#00474b',
+      'green-active': '#7e5bef',
+      'green-text-input': '#03393f',
+      'green-hover': '#9ee8df',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
     extend: {},
   },
   plugins: [],
