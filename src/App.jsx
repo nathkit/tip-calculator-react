0592e59bca2805db040b1ecd570000227f93ddda
Nathkit_Nae <nathkit101@gmail.com>
2023-08-26 10:14:22 +0700

    feat:add logic state and style CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,135 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [totleBill, setTotalBill] = useState("");
+  const [percentTip, setPercentTip] = useState(0);
+  const [numberPeople, setNumberPeople] = useState("");
+  const [tipAmount, setTipAmount] = useState("0.00");
+
+  const calculateTipAmount = (bill, tip, numberPeople) => {
+    const result = (Number(bill) * tip) / numberPeople;
+    setTipAmount(result.toFixed(2));
+  };
+
+  const handleBill = (e) => {
+    setTotalBill(e.target.value);
+    console.log(totleBill);
+  };
+
+  const handleTip = (e) => {
+    setPercentTip(e.target.value);
+  };
+
+  const handleNumberPeople = (e) => {
+    setNumberPeople(e.target.value);
+  };
+
+  useEffect(() => {
+    if (totleBill !== "" && percentTip !== 0 && numberPeople !== "")
+      calculateTipAmount(totleBill, percentTip, numberPeople);
+  }, [totleBill, percentTip, numberPeople]);
 
   return (
     <>
-      <div className="bg-body-bg h-screen">
-        <div className="app-header">
+      <div className="bg-body-bg h-screen flex flex-col items-center justify-center">
+        <div className="app-header mb-14 sm:mb-20">
           <h1>S P L I</h1>
           <h1>T T E R</h1>
         </div>
-        <div className="calculator-container min-w-[">
+        <div className="calculator-container bg-[white] w-[925px] h-[485px] flex justify-around items-center">
           <div className="input-container">
             <div className="bill-input">
               <h1>Bill</h1>
-              <input className="input" type="text" placeholder="$" />
+              <input
+                className="input"
+                type="number"
+                min="0"
+                placeholder="$"
+                onChange={(e) => handleBill(e)}
+              />
             </div>
             <div className="tip-select-section">
               <h1>Select Tip %</h1>
-              <div className="tip-btn-container">
-                <button className="tip-btn">5%</button>
-                <button className="tip-btn">10%</button>
-                <button className="tip-btn">15%</button>
-                <button className="tip-btn">25%</button>
-                <button className="tip-btn">50%</button>
+              <div className="tip-btn-container grid grid-cols-2 gap-y-3.5 sm:grid-cols-3">
+                <button
+                  className="tip-btn"
+                  value={0.05}
+                  onClick={(e) => {
+                    handleTip(e);
+                  }}>
+                  5%
+                </button>
+                <button
+                  className="tip-btn"
+                  value={0.1}
+                  onClick={(e) => {
+                    handleTip(e);
+                  }}>
+                  10%
+                </button>
+                <button
+                  className="tip-btn"
+                  value={0.15}
+                  onClick={(e) => {
+                    handleTip(e);
+                  }}>
+                  15%
+                </button>
+                <button
+                  className="tip-btn"
+                  value={0.25}
+                  onClick={(e) => {
+                    handleTip(e);
+                  }}>
+                  25%
+                </button>
+                <button
+                  className="tip-btn"
+                  value={0.5}
+                  onClick={(e) => {
+                    handleTip(e);
+                  }}>
+                  50%
+                </button>
                 <div className="tip-btn">
-                  <input type="text" placeholder="Custom" />
+                  <input
+                    type="number"
+                    min="0"
+                    placeholder="Custom"
+                    value={percentTip}
+                    onChange={(e) => {
+                      handleTip(e);
+                    }}
+                  />
                 </div>
               </div>
             </div>
             <div className="people-count">
               <h1>Number of People</h1>
-              <input className="input" type="text" />
+              <input
+                className="input"
+                type="number"
+                min="0"
+                onChange={(e) => {
+                  handleNumberPeople(e);
+                }}
+              />
             </div>
           </div>
-          <div className="output-container">
-            <div className="calulate-output">
+          <div className="output-container bg-green-dark">
+            <div className="calculate-output">
               <div className="label">
                 <h1>Tip Amount</h1>
                 <p>/ person</p>
               </div>
               <div className="display-output">
-                <h1>$0.00</h1>
+                <h1>${tipAmount}</h1>
               </div>
             </div>
-            <div className="calulate-output">
+            <div className="calculate-output">
               <div className="label">
                 <h1>Total</h1>
                 <p>/ person</p>
@@ -56,7 +138,9 @@ function App() {
                 <h1>$0.00</h1>
               </div>
             </div>
-            <div className="reset-btn">RESET</div>
+            <button className="reset-btn">
+              <h1>RESET</h1>
+            </button>
           </div>
         </div>
         <div class="attribution">
